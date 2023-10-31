342b02f7b79756fa07f02dbff6e76e784cb4d20a
jstnliu <jlcheeseburger@gmail.com>
2023-10-31 15:37:33 -0700

    part 2 complete

--- main_app/views.py
+++ main_app/views.py
@@ -26,6 +26,7 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_idx(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches,
     })
