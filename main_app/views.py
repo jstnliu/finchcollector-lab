a8cb867ef3ef07e6ab59447e4867a23be18b9271
jstnliu <jlcheeseburger@gmail.com>
2023-10-31 15:22:53 -0700

    update models.py, urls.py, views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
+# import Finch model   
+from .models import Finch
 
-# BABY STEP: 
-finches = [
-    {
-    'name': 'Atticus',
-    'breed': 'Hoary Redpoll',
-    'description': 'Atticus is a very logical and practical, almost like he was a lawyer in his past life...',
-    'age': 7,
-    },
-    {
-    'name': 'Vegeta',
-    'breed': 'Japanese grosbeak',
-    'description': 'A feisty finch, he always seems like he\'s trying go further beyond...',
-    'age': 5,
-    },
-]
+# # BABY STEP: 
+# finches = [
+#     {
+#     'name': 'Atticus',
+#     'breed': 'Hoary Redpoll',
+#     'description': 'Atticus is a very logical and practical, almost like he was a lawyer in his past life...',
+#     'age': 7,
+#     },
+#     {
+#     'name': 'Vegeta',
+#     'breed': 'Japanese grosbeak',
+#     'description': 'A feisty finch, he always seems like he\'s trying go further beyond...',
+#     'age': 5,
+#     },
+# ]
 
 # Create your views here.
 def home(request):
@@ -26,4 +28,10 @@ def about(request):
 def finches_idx(request):
     return render(request, 'finches/index.html', {
         'finches': finches,
+    })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id = finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch
     })
