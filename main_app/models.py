a336b0b359a7cba4edf803e8340094375860f221
jstnliu <jlcheeseburger@gmail.com>
2023-11-01 23:34:50 -0700

    parts 3 and 4 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 # import reverse function 
 from django.urls import reverse
+# import date function
+from datetime import date
 # Create your models here.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length = 100)
     breed = models.CharField(max_length = 100)
@@ -15,3 +24,25 @@ class Finch(models.Model):
         return reverse('detail', kwargs = {
             'finch_id': self.id
         })
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date = date.today()).count() >= len(MEALS)
+    
+class Feeding(models.Model):
+    date = models.DateField('feeding Date')
+    meal = models.CharField(
+        max_length = 1,
+        choices = MEALS,
+        default = MEALS[0][0],
+    )
+    # finch_id foreign key
+    finch = models.ForeignKey(
+        Finch,
+        on_delete = models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # import from the Django framework
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # import Finch model   
 from .models import Finch
-
-# # BABY STEP: 
-# finches = [
-#     {
-#     'name': 'Atticus',
-#     'breed': 'Hoary Redpoll',
-#     'description': 'Atticus is a very logical and practical, almost like he was a lawyer in his past life...',
-#     'age': 7,
-#     },
-#     {
-#     'name': 'Vegeta',
-#     'breed': 'Japanese grosbeak',
-#     'description': 'A feisty finch, he always seems like he\'s trying go further beyond...',
-#     'age': 5,
-#     },
-# ]
+# import FeedingForm from forms.py
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -35,11 +21,36 @@ def finches_idx(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id = finch_id)
+    # instantiate FeedingForm to be rendered in template
+    feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
-        'finch': finch
+        'finch': finch,
+        # include feeding_form in context
+        'feeding_form': feeding_form
     })
+
+def add_feeding(request, finch_id):
+    # create ModelForm instance using data in 'request.POST'
+    form = FeedingForm(request.POST)
+    # validate form
+    if form.is_valid():
+        # don't save form to DB until it
+        # has an assigned 'finch_id'
+        new_feeding = form.save(commit = False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id = finch_id)
 
 class FinchCreate(CreateView):
     model = Finch
     fields = '__all__'
   
+class FinchUpdate(UpdateView):
+    model = Finch
+    # let all but 'name' be editable
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    # redirect to index page
+    success_url = '/finches'
